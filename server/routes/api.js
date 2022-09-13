87bc1093cdb0bb7eb92f9463c14111309818217e
LukasMercak <lukasthevicious@gmail.com>
2022-09-13 21:14:13 +0200

    server

---
+++ server/routes/api.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+//Routes
+router.get("/", (req, res) => {});
+router.post("/save", (req, res) => {});
+router.get("/name", (req, res) => {});
+
+module.exports = router;
