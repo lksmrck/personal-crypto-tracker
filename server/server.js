f9fcfc74d3a54491f5cadd035b5d2b69cf113c2c
LukasMercak <lukasthevicious@gmail.com>
2022-09-13 21:11:26 +0200

    server

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+const routes = require("./routes/api");
+
+const MONGODB_URI = "doplnit adresu databaze";
+
+//Pripojeni na databazi
+mongoose.connect(MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  //kdyz se connection provede, napise:
+  console.log("Mongoose is connected!");
+});
+
+//Data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//CORS policy handler
+app.use(cors());
+
+// /api jako starting route
+app.use("/api", routes);
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
